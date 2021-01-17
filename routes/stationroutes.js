81acf903b5e72a7fa3cd98ea91a767da6d4a9735
Sanjana Kumari <sanjana01999@gmail.com>
2021-01-17 15:22:28 +0530

    nearest stations

--- routes/stationroutes.js
+++ routes/stationroutes.js
@@ -18,7 +18,16 @@ router.route('/neareststations').post(function(req,res){
             return res.status(500).send();
         }
         var nearest = [];
-        nearest.push(station[0],station[1]);
+        for(var i=0 ;i < station.length;i++){
+            var ky = 40000 / 360;
+            var kx = Math.cos(Math.PI * req.body.coordinates[0].latitude / 180.0) * ky;
+            var dx = Math.abs(req.body.coordinates[0].longitude - station[i].coordinates.longitude) * kx;
+            var dy = Math.abs(req.body.coordinates[0].latitude - station[i].coordinates.latitude) * ky;
+            var val = Math.sqrt(dx * dx + dy * dy);
+            if (val <= (req.body.radius)){
+                nearest.push(station[i]);
+            }
+        }
         return res.json(nearest);
     });
 });
